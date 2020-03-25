73d976933fb7fd60b82c465390ae354227ae8286
shohedul350 <shohedulislamemon@gmail.com>
2020-03-26 00:56:12 +0600

    file structure and package manager added

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+
+mongoose.connection.once('open', () => {
+  console.log('mongodb connection establish succesfully');
+});
+app.listen(port, () => { console.log(`The app is running on port ${port}`); });
