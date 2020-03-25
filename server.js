289c84c5db2b16e897ed3b53365ace941b285193
shohedul350 <shohedulislamemon@gmail.com>
2020-03-26 02:22:41 +0600

    modified

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const uri = process.env.ATLAS_URI;
+// router
+app.use('/api', require('./routes/userRoute'));
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
